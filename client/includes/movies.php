463077dc05886e841df8b3724daae451cd07faae
John Absolu <Johnson.absolu@hotmail.com>
2024-06-06 01:05:28 -0400

    Added Movies Includes file, and set up API to pull data from backend

---
+++ client/includes/movies.php
@@ -0,0 +1,39 @@
+<?php
+
+include '../../server/config.php';
+include '../../server/index.php';
+
+// Make call to backend
+$new_connection = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+// Check connection
+if ($new_connection->connect_error) {
+    die("Connection failed: " . $new_connection->connect_error);
+}
+
+$sql_query = 'SELECT * FROM `Movie`';
+$result = $new_connection->query($sql_query);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $title = $row["Title"]; // get the title of the movie
+        $rating = $row["Rating"]; // get the rating 
+        $length = $row["Length"]; // get duration
+        $released = $row["Released_date"]; // get released date
+        
+        //create movie listing for each movies pulled
+        echo "<div class='movie'>"; 
+        echo "<img src='client/img/164729.jpg' alt='$title'>";
+        echo "<div class='movie-info'>";
+        echo "<h3>The movie is $title</h3>";
+        echo "<p>$length</p>";
+        echo "<button>Get Tickets</button>";
+        echo "</div>";
+        echo "</div>";
+    }
+} else {
+    echo "No movies are listed.";
+}
+
+$new_connection->close();
+?>
