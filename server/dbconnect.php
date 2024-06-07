b6a4c4d01f29e0385e2bcaa7432a09f35635ba0b
John Absolu <Johnson.absolu@hotmail.com>
2024-06-07 17:37:13 -0400

    Switched index.php db connection file to dbconnect.php

---
+++ server/dbconnect.php
@@ -0,0 +1,27 @@
+<?php
+  error_reporting(E_ALL);
+  // ini_set('display_errors', 1);
+
+include "./config.php";
+
+//create connection
+$connection = new mysqli($DB_HOST, $DB_USER, $DB_PASS);
+
+
+//check connection
+if ($connection->connect_error) {
+  die("connection failed: " . $connection->connect_error);
+}
+
+$sql = "SHOW DATABASES LIKE '$DB_NAME'";
+$result = $connection->query($sql);
+
+if ($result->num_rows == 0) {
+  $sql_querry = "CREATE DATABASE $DB_NAME";
+  if ($connection->query($sql_querry) === TRUE) {
+    echo "Database $DB_NAME has been created";
+  } else {
+    echo "Creating Database " . $connection->error;
+  }
+}
+?>
