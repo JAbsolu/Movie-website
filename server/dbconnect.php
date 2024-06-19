18563e26d74df55838cdf2565ca4d4206a19b435
John Absolu <Johnson.absolu@hotmail.com>
2024-06-19 17:49:59 -0400

    Added and updated Schemas

--- server/dbconnect.php
+++ server/dbconnect.php
@@ -1,22 +1,10 @@
 <?php
 // The following 2 codes bellow are for error checking, remove comments to debug
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 include "./config.php";
-include "./schema/address.php";
-include "./schema/cinema.php";
-include "./schema/customer.php";
-include "./schema/employee.php";
-include "./schema/gift_card.php";
-include "./schema/location.php";
-include "./schema/movie_room.php";
-include "./schema/movie.php";
-include "./schema/order.php";
-include "./schema/payment.php";
-include "./schema/reservation.php";
-include "./schema/role.php";
-include "./schema/ticket.php";
+
 
 //create connection
 $connection = new mysqli($DB_HOST, $DB_USER, $DB_PASS);
@@ -33,9 +21,26 @@ $result = $connection->query($sql);
 if ($result->num_rows == 0) {
   $sql_querry = "CREATE DATABASE $DB_NAME";
   if ($connection->query($sql_querry) === TRUE) {
-    echo "Database $DB_NAME has been created";
+    echo "Database $DB_NAME has been created<br><br>";
   } else {
     echo "Creating Database " . $connection->error;
   }
 }
+
+include "schema/address.php";
+include "schema/location.php";
+include "schema/customer.php";
+include "schema/cinema.php";
+include "schema/employee.php";
+include "schema/food.php";
+include "schema/gift_card.php";
+include "schema/merch.php";
+include "schema/movie_room.php";
+include "schema/movie.php";
+include "schema/order.php";
+include "schema/payment.php";
+include "schema/reservation.php";
+include "schema/role.php";
+include "schema/ticket.php";
+
 ?>
