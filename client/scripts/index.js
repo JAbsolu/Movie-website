6576123adb175925a2b6dff9a04e6eb09780a92e
John Absolu <Johnson.absolu@hotmail.com>
2024-06-21 00:01:17 -0400

    Added Vanilla JS

---
+++ client/scripts/index.js
@@ -0,0 +1,16 @@
+var bannerSwiper = new Swiper('.banner-swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+      delay: 3000,
+  },
+});
+
+var showcasingSwiper = new Swiper('.showcase-swiper-container', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+      delay: 5000,
+  },
+});
